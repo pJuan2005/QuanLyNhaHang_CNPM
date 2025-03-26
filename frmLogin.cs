91cf04054221a6585f512c575e6d039bd7020c2c
Anh chuẩn đẹp zai <phamchuan2608@gmail.com>
2025-03-26 23:39:02 +0700

    Cập nhật lại login

--- frmLogin.cs
+++ frmLogin.cs
@@ -10,11 +10,96 @@ using System.Windows.Forms;
 
 namespace QuanLyNhaHang
 {
-    public partial class frmLogin : Form
+    public partial class frmDangNhap : Form
     {
-        public frmLogin()
+        public frmDangNhap()
         {
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Đăng Nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(111, 320);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(162, 35);
+            this.btnDangNhap.TabIndex = 1;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(588, 320);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(162, 35);
+            this.btnThoat.TabIndex = 1;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(216, 105);
+            this.txtTenDangNhap.Multiline = true;
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(515, 36);
+            this.txtTenDangNhap.TabIndex = 2;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(216, 202);
+            this.txtMatKhau.Multiline = true;
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(515, 36);
+            this.txtMatKhau.TabIndex = 2;
+            // 
+            // frmDangNhap
+            // 
+            this.ClientSize = new System.Drawing.Size(862, 405);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDangNhap";
+            this.Text = "DangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private Label label1;
+        private Label label2;
+        private Button btnDangNhap;
+        private Button btnThoat;
+        private TextBox txtTenDangNhap;
+        private TextBox txtMatKhau;
     }
 }
